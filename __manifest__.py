700585d62f385718b0898f287057a7b2d475c7c6
Dipika Paudel <pdldipika@gmail.com>
2025-10-21 07:58:02 +0000

    react form

--- __manifest__.py
+++ __manifest__.py
@@ -4,12 +4,23 @@
     'summary': 'Add receipt fields and printable receipt for Sale Orders',
     'category': 'Sales',
     'author': 'Dipika',
-    'depends': ['sale_management'],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/sale_order_view.xml',
-        'views/report_receipt_template.xml',
-    ],
+    #'depends': ['sale_management'],
+    'depends': ['base', 'web', 'website'],
+   # 'data': [
+    #    'security/ir.model.access.csv',
+     #   'views/sale_order_view.xml',
+      #  'views/report_receipt_template.xml',
+    #],
+    'data': ['views/menu.xml'],
+     'assets': {
+        'web.assets_frontend': [
+            'custom_sale_extension/static/src/js/form_app.js',
+            'custom_sale_extension/static/src/xml/templates.xml',
+        ],
+    },
     'installable': True,
     'application': False,
 }
+
+
+
